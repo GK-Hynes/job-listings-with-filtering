1e3a7888a0f8ae4980900fd029ed47b165e7c2b2
Gerard Hynes <gkhynes@gmail.com>
2020-09-09 21:04:38 +0100

    Adjust JobListing styles

--- src/JobListing.js
+++ src/JobListing.js
@@ -25,13 +25,13 @@ const JobListing = ({ job }) => {
         featured && `border-solid border-teal-500 border-l-4`
       } flex flex-row mx-auto mb-6 w-9/12 max-w-screen-xl bg-white shadow-xl rounded-md`}
     >
-      <div className="flex flex-row justify-center items-center p-4">
+      <div className="flex justify-center items-center p-4">
         <div className="h-20 w-20 ml-8">
           <img src={logo} alt={company} />
         </div>
       </div>
-      <div className="flex-1 py-8 px-4">
-        <div className="flex flex-row items-center text-sm">
+      <div className="py-8 px-4">
+        <div className="flex items-center text-sm">
           <h3 className="mr-4 text-desaturated-dark-cyan font-bold py-2">
             {company}
           </h3>
@@ -46,8 +46,10 @@ const JobListing = ({ job }) => {
             </span>
           )}
         </div>
-        <h2 className="text-xl font-bold mb-1 tracking-tight">{position}</h2>
-        <ul className="flex flex-row items-center text-dark-gray-cyan tracking-tight">
+        <h2 className="text-xl font-bold mb-1 tracking-tight hover:text-desaturated-dark-cyan">
+          <a href="#">{position}</a>
+        </h2>
+        <ul className="flex items-center text-dark-gray-cyan tracking-tight">
           <li className="mr-4">{postedAt}</li>
           <li className="mr-4">&#8226;</li>
           <li className="mr-4">{contract}</li>
@@ -55,15 +57,16 @@ const JobListing = ({ job }) => {
           <li className="mr-4">{location}</li>
         </ul>
       </div>
-      <div className="flex flex-row justify-center flex-1 p-4 text-sm">
-        <ul className="flex flex-row flex-wrap items-center">
-          {tags.map((tag, i) => (
-            <li key={i}>
-              <button className="mr-4 p-2 bg-light-gray-cyan-filters text-desaturated-dark-cyan font-bold rounded hover:bg-desaturated-dark-cyan hover:text-white">
-                {tag}
-              </button>
-            </li>
-          ))}
+      <div className="flex justify-center p-4 ml-auto text-sm">
+        <ul className="flex flex-wrap items-center">
+          {tags &&
+            tags.map((tag, i) => (
+              <li key={i}>
+                <button className="mr-4 p-2 bg-light-gray-cyan-filters text-desaturated-dark-cyan font-bold rounded hover:bg-desaturated-dark-cyan hover:text-white">
+                  {tag}
+                </button>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
